e7322e9924efa4ab809b5eeb2953a0ab5120b35c
rogesson <rogessonb@gmail.com>
2020-08-15 14:37:20 -0300

    horizontal moviment

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ SCREEN_SIZE = (640, 480)
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE, RESIZABLE, 32)
 clock = pygame.time.Clock()
+
+speed_x = 133
+x = 0
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -16,10 +20,11 @@ while True:
     time_passed = clock.tick()
     
     time_passed_seconds = time_passed / 1000.0
+    x += speed_x * time_passed_seconds
     
     screen.fill((0, 0, 0))
 
-    pygame.draw.circle(screen, (0, 0, 255), (0 + 10, 0 + 10), 10)
+    pygame.draw.circle(screen, (0, 0, 255), (int(x) + 10, 0 + 10), 10)
 
     pygame.display.flip()
 
