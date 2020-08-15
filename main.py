fb3d5bfb44759f85633e9cd3e5ad46ecf71ac2e3
rogesson <rogessonb@gmail.com>
2020-08-15 14:31:59 -0300

    create time tick

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ SCREEN_SIZE = (640, 480)
 
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE, RESIZABLE, 32)
-
+clock = pygame.time.Clock()
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-
+    time_passed = clock.tick()
+    
+    time_passed_seconds = time_passed / 1000.0
+    
     screen.fill((0, 0, 0))
 
     pygame.draw.circle(screen, (0, 0, 255), (0 + 10, 0 + 10), 10)
