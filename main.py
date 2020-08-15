7fa7d5c4b7a71fba9c31f92b9e0a69e13eb11455
rogesson <rogessonb@gmail.com>
2020-08-15 15:33:55 -0300

    create main class

--- main.py
+++ main.py
@@ -1,31 +1,50 @@
 import pygame
 
 from pygame.locals import *
-from sys import exit
+import sys
+from vector2 import *
 
-SCREEN_SIZE = (640, 480)
+def main():
+    SCREEN_SIZE = (640, 480)
 
-pygame.init()
-screen = pygame.display.set_mode(SCREEN_SIZE, RESIZABLE, 32)
-clock = pygame.time.Clock()
-
-speed_x = 133
-x = 0
-
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            exit()
-    time_passed = clock.tick()
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_SIZE, RESIZABLE, 32)
+    clock = pygame.time.Clock()
     
-    time_passed_seconds = time_passed / 1000.0
-    x += speed_x * time_passed_seconds
-    
-    screen.fill((0, 0, 0))
+    x = 0
+    y = 0
+    speed = 133
 
-    pygame.draw.circle(screen, (0, 0, 255), (int(x) + 10, 0 + 10), 10)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+        time_passed = clock.tick()
+        
+        time_passed_seconds = time_passed / 1000.0
+        #x += speed * time_passed_seconds
+        
+        screen.fill((0, 0, 0))
 
-    pygame.display.flip()
+        pressed_keys = pygame.key.get_pressed()
 
-pygame.quit()
+        if pressed_keys[K_LEFT]:
+            x -= speed * time_passed_seconds
+
+        if pressed_keys[K_RIGHT]:
+            x += speed * time_passed_seconds
+
+        if pressed_keys[K_UP]:
+            y -= speed * time_passed_seconds
+
+        if pressed_keys[K_DOWN]:
+            y += speed * time_passed_seconds
+
+        pygame.draw.circle(screen, (0, 0, 255), (int(x) + 10, int(y) + 10), 10)
+
+        pygame.display.flip()
+
+    pygame.quit()
+if __name__ == "__main__":
+    main()
